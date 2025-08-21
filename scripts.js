d27907918982ffc7097217b196308ad27ad28381
rouseld <rouseld12@gmail.com>
2025-08-21 14:11:54 -0600

    Inicializacion de CSS y Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log('Esta conectado los scripts')
+let hero__slider;
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    hero__slider = document.getElementsByClassName('hero__innercontainer__bottom__slider')
+    console.log('se ha cargado el dom')
+    console.log(hero__slider) 
+})
+
+console.log(hero__slider)
