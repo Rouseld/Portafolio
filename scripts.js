38346b52444f4d71473390bddf92903947461dc2
rouseld <rouseld12@gmail.com>
2025-08-21 17:02:49 -0600

    Creacion de estilos para las secciones y agregado de assets junto con el contenido

--- scripts.js
+++ scripts.js
@@ -2,9 +2,21 @@ let hero__slider;
 
 
 document.addEventListener('DOMContentLoaded',function(){
-    hero__slider = document.getElementsByClassName('hero__innercontainer__bottom__slider')
+    const result = document.getElementsByClassName('hero__innercontainer__bottom__slider')[0]
+    hero__slider = result
     console.log('se ha cargado el dom')
     console.log(hero__slider) 
+
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
 })
+
+
+function addAnimation(){
+    console.log(hero__slider)
+    hero__slider.setAttribute("data-animated", true);
+    console.log('se ha ejecutado')
+}
 
 console.log(hero__slider)
