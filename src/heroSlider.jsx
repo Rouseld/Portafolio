0354ae50f6dabdc8445db0fd47fe998d1597d6cd
rouseld <rouseld12@gmail.com>
2026-06-07 02:38:54 -0600

    Slider Component Creation

---
+++ src/heroSlider.jsx
@@ -0,0 +1,9 @@
+export function heroSlider(imgUrl,){
+    <div className="">
+        <a href="" className=""><img src="" alt="" /></a>
+        <div className="">
+            <img src="" alt="" className="" />
+            <h3 className=""></h3>
+        </div>   
+    </div>
+}
