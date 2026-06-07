f1733d23754f9988085b583b4a8cbfeba6cf5c47
rouseld <rouseld12@gmail.com>
2026-06-07 13:38:42 -0600

    HeroSlider V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
+import { HeroSlider } from './HeroSlider'
 
 function Inicio() {
   return (
+    <>
     <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
       <Helmet>  
         <meta name="description" content="Bienvenido a mi nuevo portafolio." />
@@ -10,6 +12,13 @@ function Inicio() {
       <h1 className='react_title'>🚀 ¡React y Vite están vivos!</h1>
       <p>La estructura funciona a la perfección.</p>
     </div>
+
+   <section className="section__hero__innercontainer--bottom">
+      <div className="hero__innercontainer__bottom__slider">
+        <HeroSlider imgUrl='Asset__PortafolioImg--2.png' pageUrl='DupontGarageApp.html' />
+      </div>
+    </section>
+    </>
   )
 }
 
@@ -17,7 +26,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Inicio />} />
+        <Route path="/" element={<Inicio />} >
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/heroSlider.jsx
+++ src/heroSlider.jsx
@@ -1,13 +1,17 @@
-export function heroSlider(imgUrl, pageUrl, sliderTitle, sliderTag1, sliderTag2){
+export function HeroSlider({imgUrl, pageUrl, sliderTitle, sliderTag1, sliderTag2}){
+    return(
     <div className="hero__sliderItem">
-        <a href="" className=""><img src="" alt="" /></a>
-        <div className="">
-            <img src="" alt="" className="" />
-            <h3 className=""></h3>
-            <div className="">
-                <p className="tags__regular"></p>
-                <p className="tags__regular"></p>
+        <a href={`./portafoliopages/${pageUrl}`}><img src={`../public/media/${imgUrl}`} alt="" /></a>
+        <div className="hero__slider--1-content slider-overlay--hidden">
+            <div className="hero__slider--1-innercontent">
+             <img src="../public/media/icon__link.svg" alt="" className="icon__large" />
+             <h3 className="title__h3">{sliderTitle}</h3>
+             <div className="hero__slider--1-tags">
+                <p className="tags__regular">{sliderTag1}</p>
+                <p className="tags__regular">{sliderTag2}</p>
+             </div>
             </div>
         </div>   
     </div>
+    )
 }
