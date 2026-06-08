da981c7fb5d5d3510f7fdf8421ef148b9c22a698
rouseld <rouseld12@gmail.com>
2026-06-07 19:24:16 -0600

    Quick sync

--- src/HeroSlider.jsx
+++ src/HeroSlider.jsx
@@ -1,10 +1,10 @@
 export function HeroSlider({imgUrl, pageUrl, sliderTitle, sliderTag1, sliderTag2}){
     return(
     <div className="hero__sliderItem">
-        <a href={`./portafoliopages/${pageUrl}`}><img src={`../public/media/${imgUrl}`} alt="" /></a>
+        <a href={`./portafoliopages/${pageUrl}`}><img src={`../media/${imgUrl}`} alt="" /></a>
         <div className="hero__slider--1-content slider-overlay--hidden">
             <div className="hero__slider--1-innercontent">
-             <img src="../public/media/icon__link.svg" alt="" className="icon__large" />
+             <img src="../media/icon__link.svg" alt="" className="icon__large" />
              <h3 className="title__h3">{sliderTitle}</h3>
              <div className="hero__slider--1-tags">
                 <p className="tags__regular">{sliderTag1}</p>
