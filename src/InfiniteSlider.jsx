f95174ac0e961af8512fd65cb9d723e4df288fd8
rouseld <rouseld12@gmail.com>
2026-06-10 06:24:28 -0600

    quick Sync

--- src/InfiniteSlider.jsx
+++ src/InfiniteSlider.jsx
@@ -4,38 +4,47 @@ import React,{useEffect, useRef,} from 'react'
 
 export function InfiniteSlider({SliderItems}){
     
-    const [scope,animate] = useAnimate();
-
     const connection = () =>{
         console.log('hola')
     }
 
-    const SliderItemsWithConnection = React.Children.map(SliderItems, (SliderItem) => {
-        return React.cloneElement(SliderItem, { saludar: connection });
+     const SliderItemsWithConnection = React.Children.map(SliderItems, (SliderItem) => {
+        return React.cloneElement(SliderItem, { PauseAnimation: PauseAniamtion, PlayAnimation:PlayAnimation });
     });
 
+    const [scope,animate] = useAnimate(); 
     const remoteControl = useRef(null);
 
-    useEffect(() =>{
-        remoteControl.current = animate(
-            scope.current,
-            {x: ['0','-50%']},
-            {ease:'linear',duration:16,repeat:Infinity}
-        );
+    function PauseAniamtion(){
+        remoteControl.current.pause();
+    }
 
-        return () =>{
-            if (remoteControl.current){
-                remoteControl.current.stop();
+    function PlayAnimation(){
+        remoteControl.current.play();
+    }
+
+      useEffect(() =>{
+            remoteControl.current = animate(
+                scope.current,
+                {x: ['0','-50%']},
+                {ease:'linear',duration:16,repeat:Infinity}
+            );
+    
+            return () =>{
+                if (remoteControl.current){
+                    remoteControl.current.stop();
+                }
             }
-        }
-    },[animate])
+        },[animate])
+
+   
+
+
 
     return(
 
         <motion.div className="hero__innercontainer__bottom__slider" 
         ref={scope}
-        onMouseEnter={()=>remoteControl.current.pause()}
-        onMouseLeave={()=>remoteControl.current.play()}
         >       
              {SliderItemsWithConnection}
              {SliderItemsWithConnection}
